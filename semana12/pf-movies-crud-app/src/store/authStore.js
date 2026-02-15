a1a941905b7d4a3904fa4abb943573087241e2bc
Julinho HJ <wjulinho.hj@gmail.com>
2026-02-14 23:35:13 -0500

    pf-movies-crud-app, auth configurado

---
+++ semana12/pf-movies-crud-app/src/store/authStore.js
@@ -0,0 +1,50 @@
+import { create } from "zustand";
+import { supabase } from "../lib/supabase";
+
+export const useAuthStore = create((set) => ({
+  user: null,
+  loading: false,
+  error: null,
+
+  register: async (email, password) => {
+    set({ loading: true, error: null });
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      set({ error: error.message, loading: false });
+      return;
+    }
+
+    set({ user: data.user, loading: false });
+  },
+
+  login: async (email, password) => {
+    set({ loading: true, error: null });
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      set({ error: error.message, loading: false });
+      return;
+    }
+
+    set({ user: data.user, loading: false });
+  },
+
+  logout: async () => {
+    await supabase.auth.signOut();
+    set({ user: null });
+  },
+
+  checkSession: async () => {
+    const { data } = await supabase.auth.getSession();
+    set({ user: data.session?.user || null });
+  },
+}));
